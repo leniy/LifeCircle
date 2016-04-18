7444bce19ec9c3a406fea86d03960e1182dc9a8e
Leniy Tsan <m@leniy.org>
2016-04-18 18:27:50 +0800

    remove BackBar
    移除顶部返回条

--- common_res/lifecircleapi/common.js
+++ common_res/lifecircleapi/common.js
@@ -7,14 +7,31 @@ function initContent(){
 		contentType : "application/json",
 		async : false,
 		success : function(data) {
+			
 			data =eval(data)[0];
 				var html = template('template', data);
+				html = replaceStr(html);
 				document.getElementById('lifecirclebody').innerHTML = html;
+				
 		},
 		error : function(e) {
 			console.log(e);
 		}
 	});
+}
+
+function replaceStr(str){
+	var html = '';
+	html=str.replace(/&#60;/g, "<");
+	html =html.replace(/&#62;/g, ">");
+	html =html.replace(/&#160;/g, "");
+	html =html.replace(/&#38;/g, "&");
+	html =html.replace(/&#34;/g, "\"");
+	html =html.replace(/&#39;/g, "'");
+	html =html.replace(/&#215;/g, "x");
+	html =html.replace(/&#247;/g, "/");
+
+	return html;
 }
 
 $(document).ready(function(){
